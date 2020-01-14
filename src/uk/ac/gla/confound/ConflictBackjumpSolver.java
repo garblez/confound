2800fd9f4094fa81495b6264fb4ff318c5fa3f85
Max Kolle <2247156k@student.gla.ac.uk>
2020-01-14 13:41:02 +0000

    Add ConflictSet class for managing a record of last checked against variables. Implement ConflictBackjumpSolver

---
+++ src/uk/ac/gla/confound/ConflictBackjumpSolver.java
@@ -0,0 +1,13 @@
+package uk.ac.gla.confound;
+
+public class ConflictBackjumpingSolver extends Solver {
+    @Override
+    public int label(int i) {
+        return 0;
+    }
+
+    @Override
+    public int unlabel(int i) {
+        return 0;
+    }
+}
---
+++ src/uk/ac/gla/confound/ConflictSet.java
@@ -0,0 +1,4 @@
+package uk.ac.gla.confound;
+
+public class ConflictSet {
+}
