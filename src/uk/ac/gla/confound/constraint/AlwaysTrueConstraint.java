350be0edd1e525c1bbf2bc39caa244b9ab8e198b
Max Kolle <2247156k@student.gla.ac.uk>
2020-02-05 16:39:07 +0000

    Remove AlwaysTrueConstraint, IndexPair

--- src/uk/ac/gla/confound/constraint/AlwaysTrueConstraint.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.gla.confound.constraint;
-
-public class AlwaysTrueConstraint extends Constraint {
-
-    // This constraint does not record any variable indices or problem, it just exists to return true on check()
-    public AlwaysTrueConstraint() {
-        super();
-    }
-}
